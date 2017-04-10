1b12e0559d2f995c5430e2561ebac9cbf02cfe25
Thilo Goetz <twgoetz@gmx.de>
2017-04-10 07:53:34 +0200

    Coding new bootstrap grammar, not working yet.

--- src/main/scala/org/milvus/packrat/BootstrapGrammar.scala
+++ src/main/scala/org/milvus/packrat/BootstrapGrammar.scala
@@ -1,5 +1,10 @@
 package org.milvus.packrat
 
+import java.io.File
+
+import scala.collection.mutable
+import scala.io.Source
+
 sealed trait Expr
 
 case class Sym(name: String) extends Expr
@@ -21,6 +26,112 @@ case class Plus(e: Expr) extends Expr
 case class Rl(lhs: Sym, rhs: Expr)
 
 object BootstrapGrammar {
+  
+  def readRulesFromFile(file: File): Seq[Rl] = {
+    Source
+      .fromFile(file)
+      .getLines()
+      .filter(_.trim.length > 0)
+      .map(line2Rule)
+      .toSeq    
+  }
+  
+  def line2Rule(line: String): Rl = {
+    val start1 = consumeWS(line, 0)
+    val (start2, lhs) = readSymbol(line, start1)
+    val start3 = consumeWS(line, start2)
+    assert(line.charAt(start3) == '-')
+    assert(line.charAt(start3 + 1) == '>')
+    val start4 = consumeWS(line, start3 + 2)
+    val (start5, expr) = readExpression(line, start4)
+    val start6 = consumeWS(line, start5)
+    assert(start6 == line.length)
+    Rl(Sym(lhs), expr)
+  }
+  
+  def readExpression(s: String, pos: Int): (Int, Expr) = {
+    assert(s.length > pos)
+    s.charAt(pos) match {
+      case '\'' => {
+        val (i0, c0) = readCharacter(s, pos + 1)
+        var i = i0
+        var c = c0
+        val sb = new StringBuilder()
+        while (c != '\'') {
+          sb.append(c)
+          val (i1, c1) = readCharacter(s, i)
+          i = i1
+          c = c1
+        }
+        (i + 1, Terms(sb.toString))
+      }
+      case _ => {
+        val (next, sym) = readSymbol(s, pos)
+        if (s.charAt(next) == '(') {
+          sym match {
+            case "Sq" => {
+              val (i, args) = readArgs(s, next + 1)
+              (i, Sq(args))
+            }
+          }
+        } else {
+          (next, Sym(sym))
+        }
+      }
+    }
+  }
+  
+  def readArgs(s: String, pos: Int): (Int, Array[Expr]) = {
+    def readArgs(i: Int, es: List[Expr]): (Int, List[Expr]) = {
+      val start0 = consumeWS(s, i)
+      val (start1, expr) = readExpression(s, start0)
+      val start2 = consumeWS(s, start1)
+      s.charAt(start2) match {
+        case ')' => (start2 + 1, es.reverse)
+        case ',' => readArgs(start2 + 1, expr::es)
+      }
+    }
+    val (outPos, argList) = readArgs(pos, Nil)
+    (outPos, argList.toArray)
+  }
+  
+  def readCharacter(s: String, pos: Int): (Int, Char) = {
+    var i = pos
+    val c = s.charAt(i)
+    val resChar: Char = c match {
+      case '\\' => {
+        i = i + 1
+        val a = s.charAt(i)
+        a match {
+          case 'n' => '\n'
+          case 't' => '\t'
+          case _ => a
+        }
+      }
+      case _ => c
+    }
+    i = i + 1
+    assert(s.charAt(i) == '\'')
+    (i + 1, resChar)
+  }
+  
+  def readSymbol(s: String, pos: Int): (Int, String) = {
+    val buf = mutable.Buffer[Char]()
+    var i = pos
+    while (i < s.length && s.charAt(i).isLetter) {
+      buf.append(s.charAt(i))
+      i = i + 1
+    }
+    (i, new String(buf.toArray))
+  }
+  
+  def consumeWS(s: String, pos: Int): Int = {
+    var i = pos
+    while (i < s.length && s.charAt(i).isWhitespace) {
+      i = i + 1
+    }
+    i
+  }
   
   val exprName = "Expr"
   val parenExprName = "ParenExpr"
