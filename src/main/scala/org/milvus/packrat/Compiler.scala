6aa47db62a7443067e574e8b44392423c1de5db6
Thilo Goetz <tgoetz@de.ibm.com>
2018-02-05 12:39:23 +0100

    Compiler bug fixes.

--- src/main/scala/org/milvus/packrat/Compiler.scala
+++ src/main/scala/org/milvus/packrat/Compiler.scala
@@ -9,11 +9,11 @@ import com.sun.org.apache.xpath.internal.operations.Variable
 import org.scalafmt.config.ScalafmtConfig
 
 case class Compiler(packageName: String, parserClassName: String, parseTreeClassName: String) {
-
+  
   val bufferClass = s"mutable.Buffer[$parseTreeClassName]"
-
+  
   case class TranslationResult(trans: String, fromCount: Int)
-
+  
   val staticImports =
     """import scala.collection.mutable
       |
@@ -56,7 +56,7 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
       |
       |
       |""".stripMargin
-
+  
   // Gather symbols that have no rules defined for them. Those symbols are references to externally
   // generated input, such as POS tags. Assign a (negative) integer to each such symbol, and
   // provide conversion utilities.
@@ -70,8 +70,8 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
       e match {
         case Sym(name) => if (grammarSymbols.contains(name)) Seq() else Seq(name)
         case Opt(ex) => collect(ex)
-        case Alt(ex @ _*) => ex.flatMap(collect)
-        case Sq(ex @ _*) => ex.flatMap(collect)
+        case Alt(ex@_*) => ex.flatMap(collect)
+        case Sq(ex@_*) => ex.flatMap(collect)
         case Star(ex) => collect(ex)
         case Plus(ex) => collect(ex)
         case Gap(ex) => collect(ex)
@@ -87,22 +87,30 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
   def compile(rules: Seq[Rl]): String = {
     val packageDecl = s"package $packageName\n\n"
     val parseTreeDecl = s"sealed trait $parseTreeClassName\n\n"
-    val ntDecl = s"abstract class NonTerminal(val name: String, val dtrs: Seq[$parseTreeClassName]) extends ShallowParse\n"
+    val ntDecl =
+      s"abstract class NonTerminal(val name: String, val dtrs: Seq[$parseTreeClassName]) extends " +
+        "ShallowParse\nobject NonTerminal { def unapply(nt: NonTerminal): Option[(String, " +
+        "Seq[ShallowParse])] = Some((nt.name, nt.dtrs)) }\n"
     val positionDecl = s"case class Position(pos: Int) extends $parseTreeClassName\n\n"
     val categoriesDecl = ntDecl + rules
       .map(_.lhs.name)
-      .map(name => s"case class $name(override val dtrs: $parseTreeClassName*) extends NonTerminal(" + "\"" + name + "\", dtrs)\n")
+      .map(name => s"case class $name(override val dtrs: $parseTreeClassName*) extends " +
+        s"NonTerminal(" + "\"" + name + "\", dtrs)\n")
       .mkString + positionDecl
-//    val classDecls = s"case class Position(pos: Int) extends $parseTreeClassName\n\n" +
-//      s"case class Result(success: Boolean, pos: Int, cats: mutable.Buffer[$parseTreeClassName])\n" +
-//      "val failure = Result(false, 0, mutable.Buffer())\n\n"
-
-    val classDecls = s"sealed abstract class Result(val success: Boolean, val pos: Int, val cats: mutable.Buffer[$parseTreeClassName])\n" +
-      s"case class Success(override val pos: Int, override val cats: mutable.Buffer[$parseTreeClassName]) extends Result(true, pos, cats)\n" +
+    //    val classDecls = s"case class Position(pos: Int) extends $parseTreeClassName\n\n" +
+    //      s"case class Result(success: Boolean, pos: Int, cats: mutable
+    // .Buffer[$parseTreeClassName])\n" +
+    //      "val failure = Result(false, 0, mutable.Buffer())\n\n"
+    
+    val classDecls = s"sealed abstract class Result(val success: Boolean, val pos: Int, val " +
+      s"cats: mutable.Buffer[$parseTreeClassName])\n" +
+      s"case class Success(override val pos: Int, override val cats: mutable" +
+      s".Buffer[$parseTreeClassName]) extends Result(true, pos, cats)\n" +
       "case object Failure extends Result(false, 0, mutable.Buffer())\n\n"
-
-
-    val externalSymbolDecl = "sealed abstract class ExternalSymbol(val name: String, val index: Int)\n"
+    
+    
+    val externalSymbolDecl = "sealed abstract class ExternalSymbol(val name: String, val " +
+      "index: Int)\n"
     val unknownSymbolDecl = "case object UnknownSymbol extends ExternalSymbol(\"\", -1)\n"
     val extSyms = collectExternalSymbols(rules)
     val externalSymbolsDecl = extSyms
@@ -112,17 +120,21 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
       })
       .mkString("\n")
     val externalSymbolList = "Seq(" + extSyms.mkString(", ") + ")"
-    val externalSymbolMap = s"\n\nprivate val externalSymbolMap: Map[String, ExternalSymbol] = $externalSymbolList" +
+    val externalSymbolMap = s"\n\nprivate val externalSymbolMap: Map[String, ExternalSymbol] = " +
+      s"$externalSymbolList" +
       ".foldLeft(Map[String, ExternalSymbol]())((map, sym) => map + " +
       "(sym.name -> sym))\n\n"
-    val externalSymbolCodeFun = "override def codeForExternalSymbol(sym: String) = externalSymbolMap\n  " +
+    val externalSymbolCodeFun = "override def codeForExternalSymbol(sym: String) = " +
+      "externalSymbolMap\n  " +
       "  .getOrElse(sym, UnknownSymbol)\n    .index\n\n"
     val externalSymbolDefinition = externalSymbolDecl + unknownSymbolDecl + externalSymbolsDecl +
       externalSymbolMap + externalSymbolCodeFun
     val classHeader = s"\nobject $parserClassName extends Parser[$parseTreeClassName] {\n\n"
     val startSymbol = rules(0).lhs.name
-    val abstractFunImpl = s"override def parseLongest(from: Int, to: Int, input: Seq[Int]): ParseResult = {\n" +
-      s"val res = parse$startSymbol(from, to, input)\n if (res.success) ParseSuccess(res.pos, res.cats(0))\n" +
+    val abstractFunImpl = s"override def parseLongest(from: Int, to: Int, input: Seq[Int])" +
+      s": ParseResult = {\n" +
+      s"val res = parse$startSymbol(from, to, input)\n if (res.success) ParseSuccess(res.pos, res" +
+      s".cats(0))\n" +
       "else ParseFailure\n }\n\n"
     val extSymMap = extSyms
       .zipWithIndex
@@ -131,18 +143,20 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
       .map(rl => compile(rl, extSymMap))
       .mkString("\n")
     val classFooter = "}\n"
-    packageDecl + staticImports + parseTreeDecl + classHeader + categoriesDecl + classDecls + externalSymbolDefinition + abstractFunImpl +
+    packageDecl + staticImports + parseTreeDecl + classHeader + categoriesDecl + classDecls +
+      externalSymbolDefinition + abstractFunImpl +
       functionDecls + classFooter
   }
-
+  
   def compile(rule: Rl, externalSymbols: Map[String, Int]): String = {
     val lhs = rule.lhs.name
     val funHeader = s"def parse$lhs(from: Int, to: Int, input: Seq[Int]): Result = {\n val res = "
     val funBody = compile(rule.rhs, "from", externalSymbols)
-    val funFooter = s"if (res.success) Success(res.pos, $bufferClass($lhs(res.cats: _*))) else Failure\n}\n"
+    val funFooter = s"if (res.success) Success(res.pos, $bufferClass($lhs(res.cats: _*))) else " +
+      s"Failure\n}\n"
     funHeader + funBody + funFooter
   }
-
+  
   def compile(expr: Expr, fromVar: String, externalSymbols: Map[String, Int]): String = {
     expr match {
       case Range(from, to) => compileRange(from, to, fromVar)
@@ -163,67 +177,76 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
       case _ => "Failure // Unknown grammar expression\n"
     }
   }
-
+  
   def compileOptional(expr: Expr, fromVar: String, externalSymbols: Map[String, Int]): String = {
-    s"{\nval res = ${compile(expr, fromVar, externalSymbols)} if (res.success) res else Success($fromVar, $bufferClass())\n}\n"
+    s"{\nval res = ${compile(expr, fromVar, externalSymbols)} if (res.success) res else Success" +
+      s"($fromVar, $bufferClass())\n}\n"
   }
-
+  
   def compileSeq(exprs: Seq[Expr], fromVar: String, externalSymbols: Map[String, Int]): String = {
     
     val localVar = TmpPos.next + "_"
     
     val s1 = s"{\n val dtrs = mutable.Buffer[$parseTreeClassName]()\n val ${localVar}0 = $fromVar\n"
-
+    
     def cs(start: Int, end: Int): String = {
       if (start == end) {
         val count = start + 1
         val c1 = s"\n val res$count = ${compile(exprs(start), localVar + start, externalSymbols)}"
-        val c2 = s"if (res$count.success) {\n dtrs ++= res$count.cats\n Success(res$count.pos, dtrs)\n } else {\n Failure\n }\n"
+        val c2 = s"if (res$count.success) {\n dtrs ++= res$count.cats\n Success(res$count.pos, " +
+          s"dtrs)\n } else {\n Failure\n }\n"
         c1 + c2
       } else {
         val count = start + 1
         val c1 = s"val res$count = ${compile(exprs(start), localVar + start, externalSymbols)}"
-        val c2 = s"if (res$count.success && res$count.pos < input.size) {\n dtrs ++= res$count.cats\n val $localVar$count = res$count.pos\n"
+        val c2 = s"if (res$count.success && res$count.pos < input.size) {\n dtrs ++= res$count" +
+          s".cats\n val $localVar$count = res$count.pos\n"
         val c4 = cs(start + 1, end)
         val c5 = "}\n else Failure\n"
         c1 + c2 + c4 + c5
       }
     }
-
+    
     val s2 = "}\n"
     s1 + cs(0, exprs.size - 1) + s2
   }
-
+  
   def compileSet(set: Array[Int], fromVar: String): String = {
     util.Arrays.sort(set)
     if (set.isEmpty) "Failure"
     else {
-
+      
       def cs(start: Int, end: Int): String = {
         val size = end - start
-        if (size == 1) s"if (ch == ${set(start)}) Success($fromVar + 1, $bufferClass(Position($fromVar))) else Failure\n"
+        if (size == 1) s"if (ch == ${set(start)}) Success($fromVar + 1, $bufferClass(Position" +
+          s"($fromVar))) else Failure\n"
         else if (size == 2) {
-          val c1 = s"if (ch == ${set(start)}) Success($fromVar + 1, $bufferClass(Position($fromVar)))\n"
-          val c2 = s"else if(ch == ${set(start + 1)}) Success($fromVar + 1, $bufferClass(Position($fromVar)))\n else Failure\n "
+          val c1 = s"if (ch == ${set(start)}) Success($fromVar + 1, $bufferClass(Position" +
+            s"($fromVar)))\n"
+          val c2 = s"else if(ch == ${set(start + 1)}) Success($fromVar + 1, $bufferClass(Position" +
+            s"($fromVar)))\n else Failure\n "
           c1 + c2
         } else {
           val middlePos = start + (size / 2)
           val dec = set(middlePos)
           val c1 = s"if (ch < $dec) {\n ${cs(start, middlePos)} }\n"
           val c2 = s"else if (ch > $dec) {\n ${cs(middlePos + 1, end)} } \n"
-          val c3 = s"else if (ch == $dec) Success($fromVar + 1, $bufferClass(Position($fromVar)))\n else Failure\n"
+          val c3 = s"else if (ch == $dec) Success($fromVar + 1, $bufferClass(Position($fromVar)))" +
+            s"\n else Failure\n"
           c1 + c2 + c3
         }
       }
-
+      
       s"{\nval ch = input($fromVar)\n" + cs(0, set.size) + "}\n"
     }
   }
-
+  
   def compileStar(expr: Expr, fromVar: String, externalSymbols: Map[String, Int]): String = {
     val pos = TmpPos.next
-    val p1 = s"{\nval dtrs = mutable.Buffer[$parseTreeClassName]()\n var $pos = $fromVar\n var keepGoing = true\n while (keepGoing && $pos < to) {\n"
-    val p2 = s"val res = ${compile(expr, pos, externalSymbols)} if (!res.success) {\n keepGoing = false\n}\n"
+    val p1 = s"{\nval dtrs = mutable.Buffer[$parseTreeClassName]()\n var $pos = $fromVar\n var " +
+      s"keepGoing = true\n while (keepGoing && $pos < to) {\n"
+    val p2 = s"val res = ${compile(expr, pos, externalSymbols)} if (!res.success) {\n keepGoing =" +
+      s" false\n}\n"
     val p3 = s"else {\n dtrs ++= res.cats\n $pos = res.pos\n}\n}\n Success($pos, dtrs)\n}\n"
     p1 + p2 + p3
   }
@@ -233,8 +256,10 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
     val expression = compile(expr, fromVar, externalSymbols)
     val p1 = s"{\n val res = $expression"
     val p2 = "if (!res.success) {\n res\n} else {\n "
-    val p3 = s"val dtrs = res.cats\n var $pos = res.pos\n var keepGoing = true\n while (keepGoing && $pos < to) {\n"
-    val p4 = s"val res = ${compile(expr, pos, externalSymbols)} if (!res.success) {\n keepGoing = false\n} else {\n"
+    val p3 = s"val dtrs = res.cats\n var $pos = res.pos\n var keepGoing = true\n while (keepGoing" +
+      s" && $pos < to) {\n"
+    val p4 = s"val res = ${compile(expr, pos, externalSymbols)} if (!res.success) {\n keepGoing =" +
+      s" false\n} else {\n"
     val p5 = s"dtrs ++= res.cats\n $pos = res.pos\n}\n}\n Success($pos, dtrs)\n}\n}\n"
     p1 + p2 + p3 + p4 + p5
   }
@@ -243,31 +268,34 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
     val pos = TmpPos.next
     val expression = compile(expr, pos, externalSymbols)
     val p1 = s"{\n var $pos = $fromVar\nvar res = $expression"
-    val p2 = s"while (!res.success && $pos < to) {\n "
+    val p2 = s"while (!res.success && ($pos + 1) < to) {\n "
     val p3 = s"$pos = $pos + 1\nres = $expression}\nres\n}\n"
     p1 + p2 + p3
   }
   
-  def compileAlternative(exprs: Seq[Expr], fromVar: String, externalSymbols: Map[String, Int]): String = {
-
+  def compileAlternative(exprs: Seq[Expr], fromVar: String, externalSymbols: Map[String, Int])
+  : String = {
+    
     val max = exprs.size - 1
-
+    
     def compileAlternative(pos: Int): String = {
       if (pos == max) {
         compile(exprs(pos), fromVar, externalSymbols)
       } else {
         val next = compileAlternative(pos + 1)
-        val a1 = s"{\n val res = ${compile(exprs(pos), fromVar, externalSymbols)} if (res.success) res\n"
+        val a1 = s"{\n val res = ${compile(exprs(pos), fromVar, externalSymbols)} if (res" +
+          s".success) res\n"
         val a2 = s"else $next}\n"
         a1 + a2
       }
     }
-
+    
     compileAlternative(0)
   }
-
+  
   def compileRange(from: Int, to: Int, fromVar: String): String = {
-    s"if ($fromVar >= input.size) Failure\nelse {\n val c = input($fromVar)\n if (c >= $from && c <= $to)\n" +
+    s"if ($fromVar >= input.size) Failure\nelse {\n val c = input($fromVar)\n if (c >= $from && c" +
+      s" <= $to)\n" +
       s"Success($fromVar + 1, $bufferClass(Position($fromVar)))\n else\n Failure}\n"
   }
   
@@ -284,24 +312,26 @@ case class Compiler(packageName: String, parserClassName: String, parseTreeClass
     }
     
   }
-
+  
 }
 
 object Compiler {
-
+  
   def main(args: Array[String]): Unit = {
     //    val packageName = "org.milvus.packrat.samples.grammar"
     //    val parserClassName = "GrammarParser"
     //    val parseTreeClassName = "GrammarParse"
-
+    
     val packageName = "org.milvus.packrat.samples"
     val parserClassName = "ShallowParser"
     val parseTreeClassName = "ShallowParse"
-
-    val inputStream = classOf[Compiler].getClassLoader.getResourceAsStream("samples/ShallowParser.peg")
-//    val inputStream = classOf[Compiler].getClassLoader.getResourceAsStream("BootstrapGrammar.peg")
+    
+    val inputStream = classOf[Compiler].getClassLoader.getResourceAsStream("samples/ShallowParser" +
+      ".peg")
+    //    val inputStream = classOf[Compiler].getClassLoader.getResourceAsStream
+    // ("BootstrapGrammar.peg")
     val rules = GrammarReader.parseGrammar(inputStream)
-
+    
     //    val rules = BootstrapGrammar.loadRules()
     rules.foreach(println(_))
     val compiler = Compiler(packageName, parserClassName, parseTreeClassName)
@@ -310,7 +340,7 @@ object Compiler {
     val formatted = org.scalafmt.Scalafmt.format(unformatted, config).get
     println(formatted)
   }
-
+  
 }
 
 object BootstrapCompiler {
@@ -318,7 +348,7 @@ object BootstrapCompiler {
     val packageName = "org.milvus.packrat.samples.grammar"
     val parserClassName = "GrammarParser"
     val parseTreeClassName = "GrammarParse"
-
+    
     val rules = BootstrapGrammar.loadRules()
     rules.foreach(println(_))
     val compiler = Compiler(packageName, parserClassName, parseTreeClassName)
@@ -328,7 +358,8 @@ object BootstrapCompiler {
     println(formatted)
     
     val outFile = new File("src/main/scala/org/milvus/packrat/samples/grammar/GrammarParser.scala")
-    val out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile), StandardCharsets.UTF_8))
+    val out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile),
+      StandardCharsets.UTF_8))
     out.write(formatted)
     out.close()
   }
